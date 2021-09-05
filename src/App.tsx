460940315eb7365e8b24ef2943e0130f2c335dbe
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2021-09-05 13:57:56 +0300

    Finished with first lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import classes from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.app}>
+      <div>
+        My React kabzda project
+      </div>
+
+      <Rating/>
+
+      <Accordion/>
     </div>
   );
-}
+};
+
+function  Rating() {
+  console.log(<Star/>);
+  return (
+    <div className={classes.starWrap}>
+      <Star/>
+      <Star/>
+      <Star/>
+      <Star/>
+      <Star/>
+    </div>
+  );
+};
+
+function Accordion() {
+  return (
+    <div className={classes.accordeon}>
+      <h3>Accordion</h3>
+
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  );
+};
+
+function Star() {
+  return (<div className={classes.star}>
+
+  </div>);
+};
 
 export default App;
